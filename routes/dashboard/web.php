118c83de5e23471b5d580b75a50c0e51e915a083
bavly74 <bavly.eskander74@gmail.com>
2024-08-12 14:09:39 +0300

    starting permissions

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -3,6 +3,14 @@
 use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('dashboard')->name('dashboard.')->group(function(){
-    Route::get('/',[DashboardController::class,'index'])->name('index');
-});
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        Route::prefix('dashboard')->name('dashboard.')->group(function(){
+            Route::get('/',[DashboardController::class,'index'])->name('index');
+        });
+    });
+
